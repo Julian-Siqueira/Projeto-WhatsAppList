ef0d4e016e2e602a102796ed545b55540a961a99
Julian-Siqueira <juliansiqueiradev@gmail.com>
2024-10-17 09:23:00 -0300

    Criação da RecycleView

--- app/src/main/java/com/example/whatsapplist/MainActivity.java
+++ app/src/main/java/com/example/whatsapplist/MainActivity.java
@@ -7,8 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recycler_users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recycler_users = findViewById(R.id.recycle_users);
+        recycler_users.setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false));
+        recycler_users.setHasFixedSize(true);
+        //recycler_users.setAdapter();
     }
 }
