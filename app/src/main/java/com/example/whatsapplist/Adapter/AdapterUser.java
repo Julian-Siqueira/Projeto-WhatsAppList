279896d2349e5a0bb37b350b34c9d85912172c61
Julian-Siqueira <juliansiqueiradev@gmail.com>
2024-10-18 10:11:37 -0300

    Criação da Class AdapterUser

---
+++ app/src/main/java/com/example/whatsapplist/Adapter/AdapterUser.java
@@ -0,0 +1,73 @@
+package com.example.whatsapplist.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.whatsapplist.Model.User;
+import com.example.whatsapplist.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdapterUser extends RecyclerView.Adapter<AdapterUser.UserViewHolder> {
+
+    private List<User> userList = new ArrayList<>();
+
+    public AdapterUser(List<User> userList) {
+        this.userList = userList;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Creating views;
+        View itemList;
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        itemList = layoutInflater.inflate(R.layout.user_item, parent, false);
+        return new UserViewHolder(itemList);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        //Show views in the screen;
+        holder.photo.setImageResource(userList.get(position).getPhoto());
+        holder.name.setText(userList.get(position).getName());
+        holder.message.setText(userList.get(position).getMessage());
+        holder.hour.setText(userList.get(position).getHour());
+        holder.countMessage.setText(userList.get(position).getCountMessage());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    public class UserViewHolder extends RecyclerView.ViewHolder {
+
+        //View Type;
+        private ImageView photo;
+        private TextView name;
+        private TextView message;
+        private TextView hour;
+        private Button countMessage;
+
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            photo = itemView.findViewById(R.id.photo_user);
+            name = itemView.findViewById(R.id.name_user);
+            message = itemView.findViewById(R.id.message_user);
+            hour = itemView.findViewById(R.id.hour_user);
+            countMessage = itemView.findViewById(R.id.count_msg_user);
+
+        }
+    }
+}
